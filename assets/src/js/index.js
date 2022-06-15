01a33aa3d2419216cb00081cfb50bc3dd58ff4b2
Oscar <sanchez.torres.oscar.a@gmail.com>
2022-06-15 16:35:31 -0500

    feat: Se omodificó la API para obtener solo los primeros 151 pokemons y se normalizaron los datos de utilidad en un arreglo

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,12 +1,32 @@
+let pokemons = [];
 
 // Se obtienen los datos de la fuente de información
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+// const api = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+// const api = 'https://storage.googleapis.com/campus-cvs/00000000000-images-lectures/pokemons.json'
+const api = '../api/pokes.json'
 const main = () => {
-    fetch(url)
+    fetch(api)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => normalizeData(data))
+    .then(pokemons => console.log('Pokemons Kanto:' , pokemons))
 }
 
 // Se normalizan los datos
-    
+const normalizeData = (data) => {
+    data.forEach(element => {
+        const { number, name, ThumbnailImage, type, weight, height, abilities, weakness } = element;
+        const pokemon = {
+            id: number,
+            nombre: name,
+            imagen: ThumbnailImage,
+            tipo: type,
+            peso: weight,
+            tamaño: height,
+            habilidades: abilities,
+            debilidades: weakness,
+        };
+        pokemons.push(pokemon);
+    });
+    return pokemons;
+};    
 main()
